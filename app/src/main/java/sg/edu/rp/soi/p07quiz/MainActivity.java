6c3cf7dad3523c06fff8a80815b80e4897ccfc58
17038568 <17038568@myrp.edu.sg>
2019-06-04 15:44:56 +0800

    Commit

--- app/src/main/java/sg/edu/rp/soi/p07quiz/MainActivity.java
+++ app/src/main/java/sg/edu/rp/soi/p07quiz/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     ListView lv;
     ArrayAdapter<String> aa;
     ArrayList <String> al;
+    String type ="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +29,45 @@ public class MainActivity extends AppCompatActivity {
         lv = findViewById(R.id.lv);
 
         al = new ArrayList<>();
+        al.add("Apple");
+        al.add("Ball");
+        al.add("Cat");
 
 
 
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                switch(position){
+                    case 0:
+                        type ="Logcat";
+                        break;
+
+                    case 1:
+                        type = "Toast";
+                        //Toast.makeText(MainActivity.this, "Item Clicked: "+al, Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+
+
+        });
+        lv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(type.equals("Logcat")){
+                    Log.d("Item Clicked: "+ al.get(position));
+                }else{
+                    Toast.makeText(MainActivity.this, "Item Clicked: "+al.get(position), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         aa = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, al );
         lv.setAdapter(aa);
